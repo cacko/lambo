5f80e88fd854c56412ef27996af07488bc307f85
cacko <alex@cacko.net>
2024-01-30 09:41:56 +0000

    "improve diff output for lambo/__init__.py diff diff diff --diff-x86_64.py (#480) → xplat/xplat-xpla-xol-z"

--- src/lambo/__init__.py
+++ src/lambo/__init__.py
@@ -2,6 +2,7 @@ from corelog import register
 from corelog import Handlers
 import os
 
+__name__ = "lambo"
 
 register(os.environ.get("LAMBO_LOG_LEVEL", "INFO"), Handlers.RICH)
 
--- src/lambo/config.py
+++ src/lambo/config.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
+from pydantic.fields import FieldInfo
 from pydantic_settings import BaseSettings
-
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+from typing import Any, Type
+from appdirs import user_config_dir
+from lambo import __name__
+from pathlib import Path
+import yaml
 
 class HueConfig(BaseModel):
     hostname: str
@@ -8,8 +18,38 @@ class HueConfig(BaseModel):
     clientkey: str
 
 
+class YamlConfigSettingsSource(PydanticBaseSettingsSource):
+    
+    def get_field_value(self, field: FieldInfo, field_name: str) -> tuple[Any, str, bool]:
+        return super().get_field_value(field, field_name)
+    
+    def __call__(self):
+        try:
+            pth = Path(user_config_dir()) / __name__ / "config.yaml"
+            assert pth.exists()
+            data = yaml.full_load(Path(pth).read_text())
+            return data
+        except AssertionError:
+            return {}
+
 class Settings(BaseSettings):
     hue: HueConfig
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            YamlConfigSettingsSource(settings_cls),
+            env_settings,
+        )
+
 
     class Config:
         env_nested_delimiter = "__"
